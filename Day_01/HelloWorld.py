bee68fb7fd4dab01bf224eb7c1050e9c781ad58b
BabaMalik <babamalik206@gmail.com>
2024-10-16 16:05:33 +0530

    Added Python Coding  files to Day_1  branch

---
+++ Day_01/HelloWorld.py
@@ -0,0 +1,17 @@
+print("Hello World")
+
+
+def fun():
+    print("Welcome to Python Programming")
+
+print(fun())
+
+a= 1234567890.001
+print(type(a))
+
+
+# Python Identifiers
+"""
+Name of Variable, Class, Functions, Methods
+
+"""
